98a5599ea4402f93785ae705d858219e06f93815
uncenter <47499684+uncenter@users.noreply.github.com>
2024-06-08 22:12:09 -0400

    fix: improve reqwest error handling

--- src/github.rs
+++ src/github.rs
@@ -1,6 +1,7 @@
 use chrono::prelude::*;
+use color_eyre::Result;
 use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
-use reqwest::{blocking::Client, Error};
+use reqwest::blocking::Client;
 
 use self::pull_requests::{PullRequestsRepositoryPullRequests, PullRequestsRepositoryPullRequestsNodes};
 
@@ -12,22 +13,22 @@ type DateTime = chrono::DateTime<Utc>;
 #[graphql(schema_path = "src/schema.graphql", query_path = "src/query.graphql", response_derives = "Debug")]
 struct PullRequests;
 
-pub fn fetch_pull_requests(client: Client, owner: String, name: String, cursor: std::option::Option<std::string::String>) -> PullRequestsRepositoryPullRequests {
+pub fn fetch_pull_requests(client: Client, owner: String, name: String, cursor: std::option::Option<std::string::String>) -> Result<PullRequestsRepositoryPullRequests> {
 	let variables = pull_requests::Variables { owner, name, cursor };
 
-	let response_body = post_graphql::<PullRequests, _>(&client, "https://api.github.com/graphql", variables).unwrap();
+	let response_body = post_graphql::<PullRequests, _>(&client, "https://api.github.com/graphql", variables)?;
 
 	let response_data: pull_requests::ResponseData = response_body
 		.data
 		.expect("missing response data");
 
-	return response_data
+	return Ok(response_data
 		.repository
 		.expect("missing repository")
-		.pull_requests;
+		.pull_requests);
 }
 
-pub fn paginate_pull_requests(owner: String, name: String, token: String) -> Result<Vec<Option<PullRequestsRepositoryPullRequestsNodes>>, Error> {
+pub fn paginate_pull_requests(owner: String, name: String, token: String) -> Result<Vec<Option<PullRequestsRepositoryPullRequestsNodes>>> {
 	let client = Client::builder()
 		.user_agent("graphql-rust/0.10.0")
 		.default_headers(std::iter::once((reqwest::header::AUTHORIZATION, reqwest::header::HeaderValue::from_str(&format!("Bearer {}", token)).unwrap())).collect())
@@ -37,7 +38,7 @@ pub fn paginate_pull_requests(owner: String, name: String, token: String) -> Res
 	let mut prs: Vec<Option<PullRequestsRepositoryPullRequestsNodes>> = vec![];
 
 	loop {
-		let data = fetch_pull_requests(client.clone(), owner.clone(), name.clone(), cursor);
+		let data = fetch_pull_requests(client.clone(), owner.clone(), name.clone(), cursor)?;
 
 		prs.extend(
 			data.nodes
